80b4d28fe63177daf9f9b990e66bfface5633fea
Taeran <ktr504@naver.com>
2024-07-24 18:00:36 +0900

    Day07 OOP 클래스

---
+++ Python/abstraction.py
@@ -0,0 +1,20 @@
+#클래스와 인스턴스
+#추상화를 적용해 함수 만들기
+#2024.07.24
+
+#ssafy 입학식 키트
+class WelcomeKit:
+    clothes = 'T-shirts'
+    def __init__(self):
+        pass
+    
+    def getItem(): 
+        for 
+    
+    @staticmethod
+    def printItem(list):
+        for item in list:
+            print(f'웰컴키트로 {item}을 수령했습니다.')
+
+get_item_list = ['','']
+WelcomeKit.printItem(get_item_list)
